3cf524cdff4c544911a2c43218a53587f7b856c0
JosefaHerrera <josefa@linuxero.cl>
2017-05-22 12:54:50 -0400

    definir propiedades a las cajas desde js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@ var rosa= document.getElementById("rosa");
 var azul= document.getElementById("azul");
 var verde= document.getElementById("verde");
 var amarillo= document.getElementById("amarilla");
+
+/*DEFINICION DE PROPIEDADES DE LOS CUADRADOS CON EL CLASS CONTENEDOR*/
+contenedor.style.position ="relative";
+contenedor.style.backgroud = "DarkGray";
+contenedor.style.width = "600px";
+contenedor.style.padding = "50px";
