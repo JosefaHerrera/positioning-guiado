06ee7fdd378eadf95d15cc43236ee8646b2016c7
JosefaHerrera <josefa@linuxero.cl>
2017-05-22 12:51:20 -0400

    ocupando DOM en js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+//tranyéndonos los elementos que utilizaremos
+var cotenedor = document.getElementbyClassName("contenedor")[0];
+var rosa= document.getElementById("rosa");
+var azul= document.getElementById("azul");
+var verde= document.getElementById("verde");
+var amarillo= document.getElementById("amarilla");
